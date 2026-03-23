59a460f4630c526d84dca620a257c6f67f50f604
Hugo69md <hugo69md@gmail.com>
2026-03-23 08:00:00 +0100

    fix date format

--- utils/a_init/init.py
+++ utils/a_init/init.py
@@ -10,14 +10,14 @@ def init ():
     # Wait between 1 and 10 minutes before starting
     #time.sleep(random.randint(60, 600))
 
-    date = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     print(f"folder at {date}...")
     # create the necessary folder for the project inside the outputs folder
     if not os.path.exists("outputs"):
         os.mkdir("outputs")
     if not os.path.exists(f"outputs/data[{date}]"):
         os.mkdir(f"outputs/data[{date}]")
-    if not os.path.exists(f"outputs/data[{date}/pdf]"):
+    if not os.path.exists(f"outputs/data[{date}]/pdf]"):
         os.mkdir(f"outputs/data[{date}]/pdf")
 
     return date
